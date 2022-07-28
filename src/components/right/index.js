54c00d770ceba0ed16fd300d84e8a466872fac1a
Nosferatu <davonb113@gmail.com>
2022-07-28 11:59:16 -0500

    fixed href mail link

--- src/components/right/index.js
+++ src/components/right/index.js
@@ -6,16 +6,17 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 const Right = () => {
     return (
         <div className='right-container'>
-            <div className='core'>
-                <h3><FontAwesomeIcon icon={faHatWizard} className='icon' />Competencies</h3>
-                <p>Customer Service</p>
-                <p>Management</p>
-                <p>Communication</p>
-                <p>Problem Solving</p>
-                <p>Motivator</p>
-                <p>Social Responsibility</p>
-                <p>Leadership Role</p>
-                <p>Value of Ethics</p>
+
+            <div className='skills'>
+                <h3><FontAwesomeIcon icon={faBookAtlas}  className='icon'/>Skills</h3>
+                <span>React.js</span>
+                <span>JavaScript</span>
+                <span>Express.js</span>
+                <span>Node.js</span>
+                <span>mySQL</span>
+                <span>graphQL</span>
+                <span>Git</span>
+                <span>Github</span>
             </div>
 
             <div className='education'>
@@ -30,16 +31,16 @@ const Right = () => {
                 <p className='date'>February 2022 - July 2022</p>
             </div>
 
-            <div className='skills'>
-                <h3><FontAwesomeIcon icon={faBookAtlas}  className='icon'/>Skills</h3>
-                <span>React.js</span>
-                <span>JavaScript</span>
-                <span>Express.js</span>
-                <span>Node.js</span>
-                <span>mySQL</span>
-                <span>graphQL</span>
-                <span>Git</span>
-                <span>Github</span>
+            <div className='core'>
+                <h3><FontAwesomeIcon icon={faHatWizard} className='icon' />Competencies</h3>
+                <p>Customer Service</p>
+                <p>Management</p>
+                <p>Communication</p>
+                <p>Problem Solving</p>
+                <p>Motivator</p>
+                <p>Social Responsibility</p>
+                <p>Leadership Role</p>
+                <p>Value of Ethics</p>
             </div>
 
             <div className='border'>
@@ -49,7 +50,7 @@ const Right = () => {
             <footer>
                 <div className='contact'>
                     <h3><FontAwesomeIcon icon={faAddressBook} className='icon'/>Contact</h3>
-                    <a className='contact-icon' href='mailto: davonb113@gmail.com'><FontAwesomeIcon icon={faEnvelope} className='icon' />davonb113@gmail.com</a>
+                    <a className='contact-icon' href='mailto:davonb113@gmail.com'><FontAwesomeIcon icon={faEnvelope} className='icon' />davonb113@gmail.com</a>
                     <a className='contact-icon' href='tel:6157398777'><FontAwesomeIcon icon={faPhone} className='icon' />(615)-739-8777</a>
                     <a className='contact-icon' href='https://github.com/DavonHB'><FontAwesomeIcon icon={faGithub} className='icon' />DavonHB</a>
                 </div>
