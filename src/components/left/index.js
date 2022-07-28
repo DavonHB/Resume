9ae768cfe9cc201a2710f02bc204fa5dae4fbbe4
Nosferatu <davonb113@gmail.com>
2022-07-27 19:03:42 -0500

    stylign on left and right forms

--- src/components/left/index.js
+++ src/components/left/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPhone, faUser} from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBriefcase, faUser} from '@fortawesome/free-solid-svg-icons';
 
 const Left = () => {
     return (
@@ -10,12 +9,14 @@ const Left = () => {
                 <h3><FontAwesomeIcon icon={faUser} className='icon' />Profile</h3>
                 <p>I am Passionate and motivated individual who is committed to delivering high-performance work ethic while maintaining quality and integrity that actively meets my employers standards. I am adept at learning new concepts and applying the acquired knowledge observation and diligent attention to detail. Committed to reliably produce results which fulfill on the future of my employer, add value to the organization and its environment, and support the performance and success of my teammates.  </p>
             </div>
-            
-            <div className='contact'>
-                <h3>Contact</h3>
-                <a className='contact-icon' href='mailto: davonb113@gmail.com'><FontAwesomeIcon icon={faEnvelope} className='icon' />davonb113@gmail.com</a>
-                <a className='contact-icon' href='tel:6157398777'><FontAwesomeIcon icon={faPhone} className='icon' />(615)-739-8777</a>
-                <a className='contact-icon' href='https://github.com/DavonHB'><FontAwesomeIcon icon={faGithub} className='icon' />DavonHB</a>
+
+            <div className='work'>
+                <h3><FontAwesomeIcon icon={faBriefcase} className='icon' />Work History</h3>
+                <h5>Publix, 2017 - Current</h5>
+                <p>Collaborate with store manager, assistant and department managers, cashiers, replenishment specialists, and clerks regarding inventory control, stocking guidelines, production and product flow. 
+                    Enforce workplace and food safety procedures and standards.  Supervise and coordinate the activity of staff within the Grocery department and perform tasks such as, inventory counts, scheduled and random, to ensure and maintain product levels, training employees, implementing safety measures, and promoting the Publix brand to potential clients and customers through ‘premier customer service’. Conduct inventories, manage the quality and display of products.
+                    Developed and maintained a collaborative team environment which positively impacted morale, communication and overall working conditions. Consistently rank in the top 1% of employees for product stock rate and service. 
+                </p>
             </div>
         </div>
     )
--- src/components/right/index.js
+++ src/components/right/index.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faBookAtlas, faBuildingColumns, faHatWizard, faAddressBook } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Right = () => {
     return (
         <div className='right-container'>
-            Right
+            <div className='core'>
+                <h3><FontAwesomeIcon icon={faHatWizard} className='icon' />Competencies</h3>
+                <p>Customer Service</p>
+                <p>Management</p>
+                <p>Communication</p>
+                <p>Problem Solving</p>
+                <p>Motivator</p>
+                <p>Social Responsibility</p>
+                <p>Leadership Role</p>
+                <p>Value of Ethics</p>
+            </div>
+
+            <div className='education'>
+                <h3><FontAwesomeIcon icon={faBuildingColumns}  className='icon'/>Education</h3>
+                <p>Associates of Science</p>
+                <p className='date'>August 2019 - March 2021</p>
+                <p>Major in Mechanical Engineering, Columbia State College</p>
+                <p>5 Consecutive Semesters on the Dean's List</p>
+                <br />
+
+                <p>Certificate of Full Stack Development</p>
+                <p className='date'>February 2022 - July 2022</p>
+            </div>
+
+            <div className='skills'>
+                <h3><FontAwesomeIcon icon={faBookAtlas}  className='icon'/>Skills</h3>
+                <span>React.js</span>
+                <span>JavaScript</span>
+                <span>Express.js</span>
+                <span>Node.js</span>
+                <span>mySQL</span>
+                <span>graphQL</span>
+                <span>Git</span>
+                <span>Github</span>
+            </div>
+
+            <div className='border'>
+                <p>border</p>
+            </div>
+
+            <footer>
+                <div className='contact'>
+                    <h3><FontAwesomeIcon icon={faAddressBook} className='icon'/>Contact</h3>
+                    <a className='contact-icon' href='mailto: davonb113@gmail.com'><FontAwesomeIcon icon={faEnvelope} className='icon' />davonb113@gmail.com</a>
+                    <a className='contact-icon' href='tel:6157398777'><FontAwesomeIcon icon={faPhone} className='icon' />(615)-739-8777</a>
+                    <a className='contact-icon' href='https://github.com/DavonHB'><FontAwesomeIcon icon={faGithub} className='icon' />DavonHB</a>
+                </div>
+            </footer>
         </div>
     )
 }
