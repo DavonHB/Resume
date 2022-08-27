01d3a8d61f9c94ca0c689585f03e55decc16a6b9
Nosferatu <davonb113@gmail.com>
2022-08-26 19:56:26 -0500

    added link to portfolio and fixed margins in the right box

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -5,6 +5,8 @@ const Hero = () => {
         <header className='hero-container'>
             <h2 className='title'>Davon Brown</h2>
             <h3 className='title'>Software Engineer</h3> 
+
+            <a href="https://davonhbportfolio.herokuapp.com/" rel="noreferrer" target="_blank"><p className='title-p'>Click Here to Access My Portfolio</p></a>
         </header>
     )
 }
